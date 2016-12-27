c57f997c5c6aa11e3ccc530f46482827b2c8a81f
WorldWideDev <dev206@gmail.com>
2016-12-27 09:47:47 -0800

    added dev dir to repo, for now

---
+++ dev/createInvitees.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose'),
+    Invitee = mongoose.model('Invitee'),
+    headz = require('./allPeople.json');
+
+function createThem(){
+    console.log(headz, 'is heads');
+    for(var head = 0; head < headz.people.length; head++){
+        var newHead = new Invitee(headz.people[head]);
+        console.log(headz.people[head]);
+        newHead.save()
+    }
+}
+module.exports = createThem
